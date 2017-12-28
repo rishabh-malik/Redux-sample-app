fb5b4ca62d73f1c5b2593e9107079950a5e79229
rishabh-malik <rishabhmalik249@gmail.com>
2017-12-28 12:54:59 +0530

    setting up reducers

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore,applyMiddleware} from 'redux';
 import {BrowserRouter,Route} from 'react-router-dom';
-import './app.css'
+import './app.css';
+import reducers from './reducers';
 import App from './components/App';
 import Car from './components/car';
 
+const createStoreWithMiddleware=applyMiddleware()(createStore)
+
 ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
         <div>
             <Route exact path="/" component={App}></Route>
             <Route path="/car/:id" component={Car}></Route>
             </div>
     </BrowserRouter>
+    </Provider>
     , document.getElementById('root'));
