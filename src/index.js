751d2008c7d6f721551a1f4c8386c172e86108bd
rishabh-malik <rishabhmalik249@gmail.com>
2017-12-28 12:32:42 +0530

    car component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter,Route} from 'react-router-dom';
 import App from './components/App';
+import Car from './components/car';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <Route exact path="/" component={App}></Route>
+            <Route path="/car/:id" component={Car}></Route>
+            </div>
+    </BrowserRouter>
+    , document.getElementById('root'));
