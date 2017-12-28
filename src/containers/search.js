27fe10968a64dc15f18d0bdc3754efaa2f624adf
rishabh-malik <rishabhmalik249@gmail.com>
2017-12-28 12:49:13 +0530

    search functionality

--- src/containers/search.js
+++ src/containers/search.js
@@ -1,11 +1,34 @@
 import React,{Component} from 'react'
 
 class Search extends Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            keyword:''
+        }
+    }
+
+    searchCars=(event)=>{
+        event.preventDefault();
+        console.log(this.state)
+    }
+
+    handleChange=(event)=>{
+        this.setState({
+            keyword:event.target.value
+        })
+    }
+
+    componentDidMount(){
+        console.log(this.state)
+    }
+
     render(){
         return(
             <div className="main_search">
-            <form>
-                <input type="text"/>
+            <form onSubmit={this.searchCars}>
+                <input type="text" value={this.state.keyword} onChange={this.handleChange}/>
             </form>    
             </div>
         )
