e5c1007361271abbb3ffdb8336528cb5bb6a78c5
rishabh-malik <rishabhmalik249@gmail.com>
2017-12-29 11:46:54 +0530

    showing result

---
+++ src/containers/list_of_cars.js
@@ -0,0 +1,32 @@
+import React,{Component} from 'react'
+import {connect} from 'react-redux'
+class ListOfCars extends Component{
+
+    listOfCars=({cars})=>{
+        if(cars){
+            return cars.map((car)=>{
+                return (
+                    <div className="car_item">
+                    {car.model}
+                    </div>
+                )
+            })
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                {this.listOfCars(this.props)}
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    return{
+        cars:state.cars
+    }
+}
+
+export default connect(mapStateToProps,null)(ListOfCars);
